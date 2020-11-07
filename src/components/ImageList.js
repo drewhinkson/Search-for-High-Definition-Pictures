da89200c46416247259d30ed18aa8e7fa5be9408
Andrew <andrewhinkson88@gmail.com>
2020-11-06 19:59:43 -0500

    added css grid
    added css grid for styling, added imageCard component to better handle each indivual image, and styling

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,12 +1,13 @@
+import './ImageList.css'
 import React from 'react'
-
+import ImageCard from './ImageCard'
 
 const ImageList = (props) => {
    const images= props.images.map((image)=> {
-        return <img src= {image.urls.regular} alt = '' />
+        return <ImageCard  key={image.id} image={image}/>
     })
     return (
-    <div> {images} </div>
+    <div className ='image-list'> {images} </div>
     )
 } 
 
